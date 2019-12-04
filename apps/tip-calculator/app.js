54a4946d75dbb110a6df812edcae1942e2a39b33
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2019-12-04 13:24:05 -0800

    added other function

--- apps/tip-calculator/app.js
+++ apps/tip-calculator/app.js
@@ -1,8 +1,21 @@
 const tipBtn = document.querySelector('button');
 const tipAmtDisplay = document.querySelector('#tipAmount')
 const tipVal = document.querySelector('select')
+let otherIn= document.querySelector('.other')
 
-console.log(tipVal)
+tipVal.addEventListener('change',()=>{
+    if(tipVal.value==='other'){
+        
+        let newInput = `<input type='number' class="otherVal" value=0>`
+        otherIn.innerHTML=newInput
+      
+    }else{
+        return
+    }
+})
+
+
+
 tipBtn.addEventListener('click',(e)=>{
     let tipValue=document.querySelector('input').value;
     console.log(tipVal.value)
@@ -28,7 +41,21 @@ tipBtn.addEventListener('click',(e)=>{
                 totalTip = (tipValue*.25).toFixed(2);
                 tipMsg = `<h1>You should tip $${totalTip} on a $${tipValue} order!</h1>`
                 tipAmtDisplay.innerHTML=tipMsg
-                return 
+                return
+        case "other":
+                let otherDis = document.querySelector('.otherVal').value
+                if(Math.sign(otherDis)===-1){
+                    let errMsg= `<h1>Please make sure you add a valid number</h1>`
+                    tipAmtDisplay.innerHTML=errMsg;
+                    return
+                }
+                let discountAmount= otherDis/100;
+                totalTip = (tipValue*discountAmount).toFixed(2);
+                tipMsg = `<h1>You should tip $${totalTip} on a $${tipValue} order!</h1>`
+                tipAmtDisplay.innerHTML=tipMsg
+
+                return
+                
         default:
             totalTip=(tipValue*.10).toFixed(2);
             tipMsg = `<h1>You should tip $${totalTip} on a $${tipValue} order!</h1>`
