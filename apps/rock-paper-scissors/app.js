47665620a6ddfcb8eb342d4c7239660e6be5b361
vic30004 <victor_993@msn.com>
2019-12-07 17:52:30 -0800

    added scores and reset

--- apps/rock-paper-scissors/app.js
+++ apps/rock-paper-scissors/app.js
@@ -1,30 +1,72 @@
 const rpcLib = {
-    undecided: "https://jenniferdewalt.com/images/right_fist.png",
-    rock:'https://jenniferdewalt.com/images/right_fist.png',
-    scissor: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1200px-Rock-paper-scissors_%28scissors%29.png',
-    paper: 'https://toppng.com/public/uploads/preview/free-icons-slap-hand-115535377815nn6xph569.png'
-}
+  undecided: 'https://jenniferdewalt.com/images/right_fist.png',
+  rock: 'https://jenniferdewalt.com/images/right_fist.png',
+  scissor:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1200px-Rock-paper-scissors_%28scissors%29.png',
+  paper:
+    'https://toppng.com/public/uploads/preview/free-icons-slap-hand-115535377815nn6xph569.png'
+};
 
-const choices =['rock','paper','scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
-const botChoice = ()=>{
-    let choice = choices[Math.floor(Math.random()*choices.length)];
-    return choice
-}
-const buttons = document.querySelectorAll('button')
-botChoice()
-buttons.forEach(button=>button.addEventListener('click',()=>{
-    let bChoice = botChoice()
-    console.log(bChoice)
-    let pChoice=button.innerText.toLowerCase()
-    console.log(pChoice)
-    if(pChoice === 'rock' && bChoice === 'rock' || pChoice==='paper' && bChoice === 'paper' ||pChoice==='paper' && bChoice === 'paper'  ){
-        return console.log(`It's a draw`)
-    } else if(pChoice === 'paper' && bChoice === 'rock' ||pChoice==='scissors' && bChoice === 'paper'||pChoice === 'rock' && bChoice === 'scissors' ){
-        return console.log('Player wins')
-    } else if(pChoice==='scissors' && bChoice === 'rock' ||pChoice === 'paper' && bChoice === 'scissors' || pChoice === 'rock' && bChoice==='paper'){
-        return console.log('Bot wins')
-    }else{
-        return console.log('Something Went Wrong')
+const buttons = document.querySelectorAll('button');
+const bScore = document.querySelector('#bScore');
+const bPick = document.querySelector('#bPick');
+const pScore = document.querySelector('#pScore');
+const pPick = document.querySelector('#pPick');
+
+bScore.innerText = 0;
+pScore.innerText = 0;
+
+const botChoice = () => {
+  let choice = choices[Math.floor(Math.random() * choices.length)];
+  return choice;
+};
+
+const playAnimation = () => {
+  let choice = botChoice();
+  
+};
+
+const reset = () => {
+  bScore.innerText = 0;
+  pScore.innerText = 0;
+  bPick.innerText = '';
+  pPick.innerText = '';
+};
+
+buttons.forEach(button =>
+  button.addEventListener('click', () => {
+    let bChoice = botChoice();
+    console.log(bChoice);
+    let pChoice = button.innerText.toLowerCase();
+    console.log(pChoice);
+    if (
+      (pChoice === 'rock' && bChoice === 'rock') ||
+      (pChoice === 'paper' && bChoice === 'paper') ||
+      (pChoice === 'scissors' && bChoice === 'scissors')
+    ) {
+      pScore.innerText++;
+      bScore.innerText++;
+      return console.log(`It's a draw`);
+    } else if (
+      (pChoice === 'paper' && bChoice === 'rock') ||
+      (pChoice === 'scissors' && bChoice === 'paper') ||
+      (pChoice === 'rock' && bChoice === 'scissors')
+    ) {
+      pScore.innerText++;
+      return console.log('Player wins');
+    } else if (
+      (pChoice === 'scissors' && bChoice === 'rock') ||
+      (pChoice === 'paper' && bChoice === 'scissors') ||
+      (pChoice === 'rock' && bChoice === 'paper')
+    ) {
+      bScore.innerText++;
+      return console.log('Bot wins');
+    }else if(pChoice==='reset'){
+       return reset()
+    } else {
+      return console.log('Something Went Wrong');
     }
-}))
+  })
+);
