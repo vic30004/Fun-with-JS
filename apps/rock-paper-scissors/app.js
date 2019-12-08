5f0e898cc18d1c009543fc95cc058882fa4ac94e
vic30004 <victor_993@msn.com>
2019-12-07 21:29:13 -0800

    added animations and animation logic

--- apps/rock-paper-scissors/app.js
+++ apps/rock-paper-scissors/app.js
@@ -4,7 +4,9 @@ const rpcLib = {
   scissor:
     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1200px-Rock-paper-scissors_%28scissors%29.png',
   paper:
-    'https://toppng.com/public/uploads/preview/free-icons-slap-hand-115535377815nn6xph569.png'
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png',
+  rock2:
+    'https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png'
 };
 
 const choices = ['rock', 'paper', 'scissors'];
@@ -14,7 +16,10 @@ const bScore = document.querySelector('#bScore');
 const bPick = document.querySelector('#bPick');
 const pScore = document.querySelector('#pScore');
 const pPick = document.querySelector('#pPick');
-
+const pChoice = document.querySelector('.pChoice');
+const bChoice = document.querySelector('.bChoice');
+const animate = document.querySelector('#animation');
+const result = document.querySelector('#result');
 bScore.innerText = 0;
 pScore.innerText = 0;
 
@@ -23,9 +28,36 @@ const botChoice = () => {
   return choice;
 };
 
-const playAnimation = () => {
-  let choice = botChoice();
-  
+const playAnimation = (playerChoice, botChoice) => {
+  const { undecided, rock, paper, scissor, rock2 } = rpcLib;
+  pChoice.src = rock2;
+  bChoice.src = rock2;
+
+  setTimeout(() => {
+    if (playerChoice === 'rock') {
+      pChoice.src = rock2;
+      return;
+    } else if (playerChoice === 'paper') {
+      pChoice.src = paper;
+      return;
+    } else if (playerChoice === 'scissors') {
+      pChoice.src = scissor;
+      return;
+    }
+  }, 3000);
+
+  setTimeout(() => {
+    if (botChoice === 'rock') {
+      bChoice.src = rock2;
+      return;
+    } else if (botChoice === 'paper') {
+      bChoice.src = paper;
+      return;
+    } else if (botChoice === 'scissors') {
+      bChoice.src = scissor;
+      return;
+    }
+  }, 3000);
 };
 
 const reset = () => {
@@ -33,40 +65,66 @@ const reset = () => {
   pScore.innerText = 0;
   bPick.innerText = '';
   pPick.innerText = '';
+  pChoice.src = '';
+  bChoice.src = '';
+  result.innerText = '';
 };
 
 buttons.forEach(button =>
   button.addEventListener('click', () => {
     let bChoice = botChoice();
-    console.log(bChoice);
+
     let pChoice = button.innerText.toLowerCase();
-    console.log(pChoice);
-    if (
-      (pChoice === 'rock' && bChoice === 'rock') ||
-      (pChoice === 'paper' && bChoice === 'paper') ||
-      (pChoice === 'scissors' && bChoice === 'scissors')
-    ) {
-      pScore.innerText++;
-      bScore.innerText++;
-      return console.log(`It's a draw`);
-    } else if (
-      (pChoice === 'paper' && bChoice === 'rock') ||
-      (pChoice === 'scissors' && bChoice === 'paper') ||
-      (pChoice === 'rock' && bChoice === 'scissors')
-    ) {
-      pScore.innerText++;
-      return console.log('Player wins');
-    } else if (
-      (pChoice === 'scissors' && bChoice === 'rock') ||
-      (pChoice === 'paper' && bChoice === 'scissors') ||
-      (pChoice === 'rock' && bChoice === 'paper')
-    ) {
-      bScore.innerText++;
-      return console.log('Bot wins');
-    }else if(pChoice==='reset'){
-       return reset()
-    } else {
-      return console.log('Something Went Wrong');
+    if (pChoice === 'reset') {
+      return reset();
     }
+    playAnimation(pChoice, bChoice);
+    animate.classList.toggle('animationRun');
+
+    setTimeout(() => {
+      pPick.innerText = pChoice;
+      bPick.innerText = bChoice;
+
+      if (
+        (pChoice === 'rock' && bChoice === 'rock') ||
+        (pChoice === 'paper' && bChoice === 'paper') ||
+        (pChoice === 'scissors' && bChoice === 'scissors')
+      ) {
+        pScore.innerText++;
+        bScore.innerText++;
+        animate.classList.toggle('animationRun');
+        result.innerText = `It's a draw`;
+        setTimeout(() => {
+          result.innerText = '';
+        }, 1500);
+        return;
+      } else if (
+        (pChoice === 'paper' && bChoice === 'rock') ||
+        (pChoice === 'scissors' && bChoice === 'paper') ||
+        (pChoice === 'rock' && bChoice === 'scissors')
+      ) {
+        pScore.innerText++;
+        animate.classList.toggle('animationRun');
+        result.innerText = 'Player wins';
+        setTimeout(() => {
+          result.innerText = '';
+        }, 1500);
+        return;
+      } else if (
+        (pChoice === 'scissors' && bChoice === 'rock') ||
+        (pChoice === 'paper' && bChoice === 'scissors') ||
+        (pChoice === 'rock' && bChoice === 'paper')
+      ) {
+        bScore.innerText++;
+        animate.classList.toggle('animationRun');
+        result.innerText = 'Bot wins';
+        setTimeout(() => {
+          result.innerText = '';
+        }, 1500);
+        return;
+      } else {
+        return (result.innerText = 'Something Went Wrong');
+      }
+    }, 3000);
   })
 );
