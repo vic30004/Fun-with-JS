6b3ef05a689ddacff832c2bc98f2a19dcab9b2bb
vic30004 <victor_993@msn.com>
2019-12-12 20:22:30 -0800

    completed the logic for the game

--- apps/typing-test/app.js
+++ apps/typing-test/app.js
@@ -1,86 +1,108 @@
 let textLibrary = [
-        `To be successful, you have to use each day as an opportunity to improve, to be better, to get a little bit closer to your goals. It might sound like a lot of work—and with a busy schedule, next to impossible. But the best part is, the more you accomplish, the more you’ll want to do, the higher you’ll want to reach. So as long as you have the hunger for success, you will always have the power within you to achieve it.`,
-        `If you look closely, you’ll notice the energy of inspiration is all around. From the rising sun to a placid lake on a warm summer night; from our favorite books to our own reflections, the places and things from which we can draw inspiration are infinite. And the best part is that every day is a chance to capture this energy—to seek out all of life’s possibilities.`,
-      ];
-let copy = [...textLibrary]
-
+  `To be successful, you have to use each day as an opportunity to improve, to be better, to get a little bit closer to your goals. It might sound like a lot of work—and with a busy schedule, next to impossible. But the best part is, the more you accomplish, the more you’ll want to do, the higher you’ll want to reach. So as long as you have the hunger for success, you will always have the power within you to achieve it.`,
+  `If you look closely, you’ll notice the energy of inspiration is all around. From the rising sun to a placid lake on a warm summer night; from our favorite books to our own reflections, the places and things from which we can draw inspiration are infinite. And the best part is that every day is a chance to capture this energy—to seek out all of life’s possibilities.`,
+`I love javascript!`
+];
+let copy = [...textLibrary];
 
 const button = document.querySelector('button');
-const showText = document.querySelector('#type')
-const textArea = document.querySelector('textarea')
+const showText = document.querySelector('#type');
+const textArea = document.querySelector('textarea');
+const message = document.querySelector('.text');
+const wrong = document.querySelector('.wrong');
+const time = document.querySelector('.time')
+let startTime=0
+let endTime=0
+let count = 0;
+textArea.disabled = true;
 
+const typeText = arr => {
+  let text = arr[Math.floor(Math.random() * arr.length)];
+  let index = arr.indexOf(text);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
 
+  if (arr.length <= 0) {
+    button.innerText = 'Reset';
+    return (showText.innerHTML = '');
+  }
+  showText.innerText = text;
+  return text;
+};
+typeText(textLibrary);
 
+const reset = () => {
+  let test = [...copy];
+  textLibrary = test;
+  textArea.value=''
+  button.innerText = 'Start';
+  typeText(textLibrary);
+  console.log(textLibrary);
+  count=0;
+  wrong.innerHTML='';
+  time.innerText='';
 
-const typeText = (arr) =>{
-    let text = arr[Math.floor(Math.random()*arr.length)]
-    let index = arr.indexOf(text);
-    if (index >-1){
-        arr.splice(index,1);
-    }
+};
 
-    if(arr.length<=0){
-        button.innerText="Reset"
-       return showText.innerHTML=''
-       
-    }
-    showText.innerText= text
-    return text
+const hashTable = str => {
+  if (str === '') {
+    return;
+  }
+  let table = {};
+  let clearnStr = str.replace(/[^\w]/g, ' ').split(' ');
+  for (char of clearnStr) {
+    table[char] = table[char] + 1 || 1;
+    delete table[''];
+  }
+  return table;
+};
 
-}
-typeText(textLibrary)
+const playGame = () => {
+  let date = new Date();
+  startTime = date.getTime();
+  console.log(startTime)
+  button.innerText = 'Done';
+};
 
-const reset=()=>{
-    let test = [...copy];
-    textLibrary= test
-    button.innerText='Start'
-    typeText(textLibrary)
-    console.log(textLibrary)
-}
+const endGame = () => {
+  let date = new Date();
+  endTime = date.getTime();
+  console.log(startTime,endTime)  
+  let totalTime = ((endTime - startTime)/1000)
+  console.log(totalTime)
+  let str = textArea.value.split(' ').length;
+  console.log(textArea.value)
+  console.log(str)
+  let speed = Math.round((str / totalTime)*60)  
+  console.log(speed)
+  time.innerText = `You typed at ${speed} words per minute.\n`;
+};
 
-const hashTable  = (str) =>{
-    if(str === ''){
-        return 
-    }
-    let table = {};
-    let clearnStr = str.replace(/[^\w]/g,' ').split(' ');
-    for( char of clearnStr){
-        table[char] = table[char]+1||1 
-        delete table[""]
-    }
-   return table
-}
-
-const getTime = () =>{
-    let date = new Date();
-    let startTime = date.getTime();
-    button.innerText='Done'
-}
-
-button.addEventListener('click', ()=>{
-    if(button.innerText!= 'Start'){
-       getTime() 
-    }
+button.addEventListener('click', () => {
+  let correctText = hashTable(showText.innerText);
+  let userText = hashTable(textArea.value);
+ 
+  if (button.innerText === 'Start') {
+    textArea.disabled = false;
+    playGame();
+  } else if (button.innerText === 'Done') {
+    textArea.disabled = true;
+    button.innerText = 'Start';
     
-    let correctText = hashTable(showText.innerText);
-    let userText = hashTable(textArea.value);
-    let count =0;
-    console.log(correctText);
-    console.log(userText)
-
-    if(button.innerText === 'Start'){
-        button.innerText= 'Done'
-        for (text in userText){
-            if(userText[text]!= correctText[text]){
-                count++;
-                console.log(text)
-                console.log(count)
-            }
-        }
-        textArea.value=''
-        typeText(textLibrary)
-        console.log(textLibrary)
-    }else if(button.innerText === 'Reset'){
-        reset();
+    typeText(textLibrary);
+    
+    endGame();
+    for (text in userText) {
+      if (userText[text] != correctText[text]) {
+        count++;
+        console.log(count)
+        message.innerText = `You got ${count} wrong answers. Here is what you misspelled`;
+        let wrongWords = `<li>${text}</li> `;
+        wrong.innerHTML += wrongWords;
+      }
     }
-})
+  } else if (button.innerText === 'Reset') {
+    reset();
+  }
+});
