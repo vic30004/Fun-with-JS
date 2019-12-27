d834e21e5192e013b7c392698a0dabce0b96214e
vic30004 <victor_993@msn.com>
2019-12-27 11:05:37 -0800

    added win condition

--- apps/code-breaker/app.js
+++ apps/code-breaker/app.js
@@ -1,16 +1,40 @@
 const codeArea = document.querySelector(".code");
 const startBtn = document.querySelector("#start");
 const inputs = document.querySelectorAll("input")
+let score = 0;
+
 
 startBtn.addEventListener("click", () => {
-  if(startBtn.innerText==='Start'){
+  if(startBtn.innerText==='Start'|| startBtn.innerText==='Reset'){
+    codeArea.innerHTML=""
     startBtn.innerText="Guess"
     addNumInput(6)
   
     
   }else if(startBtn.innerText==="Guess"){
     const numbers = document.querySelectorAll(".numb")
-    console.log(numbers)
+    let winCondition = 0;
+    for (let i=0; i<numbers.length; i++){
+      if(numbers[i].value<numbers[i].correct){
+        numbers[i].style.background="blue";
+        numbers[i].style.color="white";
+      }else if(numbers[i].value>numbers[i].correct){
+        numbers[i].style.background="red"
+        numbers[i].style.color="white"
+      }else if (numbers[i].value==numbers[i].correct){
+        numbers[i].style.background="green"
+        numbers[i].style.color="white"
+        score++
+        winCondition++;
+        console.log(score)
+      }
+      if(winCondition === numbers.length){
+        console.log("game over")
+        startBtn.innerText='Reset'
+        score=0
+        
+      }
+    }
   }
   
 });
