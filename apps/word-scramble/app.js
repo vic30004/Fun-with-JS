73b84e660e765cfc4b1628855d5cab518220bcd9
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2019-12-30 08:59:00 -0800

    added a function to shuffle the word

--- apps/word-scramble/app.js
+++ apps/word-scramble/app.js
@@ -2,21 +2,37 @@ const wordList = ["Hello", "World", "Apple", "Zelous"];
 let copy = [...wordList];
 
 const randomWord = () => {
-  if (wordList.length > 0) {
+  if (wordList) {
     let word = wordList[Math.floor(Math.random() * wordList.length)];
     let indexWord = wordList.indexOf(word);
     if (indexWord >-1){
     wordList.splice(indexWord, 1); 
       
     }
-    
-    console.log(indexWord)
-    return console.log(word);
+    return word;
   } else {
     return;
   }
 };
 
-console.log(wordList)
-randomWord()
-console.log(wordList)
+const shuffleWord = text =>{
+    console.log(text)
+    let textSplit=text.split('');
+    var currentIndex = textSplit.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = textSplit[currentIndex];
+		textSplit[currentIndex] = textSplit[randomIndex];
+		textSplit[randomIndex] = temporaryValue;
+    }
+    return console.log(textSplit)
+}
+
+
