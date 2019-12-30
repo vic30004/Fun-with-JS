c1f579cda283a6a9de6cb4e3cfecb58897f32ac1
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2019-12-30 06:28:51 -0800

    initial setup

--- apps/code-breaker/app.js
+++ apps/code-breaker/app.js
@@ -1,15 +1,26 @@
 const codeArea = document.querySelector(".code");
 const startBtn = document.querySelector("#start");
+const inputs = document.querySelectorAll("input")
 
 
 startBtn.addEventListener("click", () => {
-  addNumInput(6);
+    if (startBtn.innerText==="Start"){
+        addNumInput(6);
   startBtn.innerText="Guess"
+    }
+  
+
+ else if(startBtn.innerText==="Guess"){
+      let correctAnswer = numToList(randomNumber())
+      playGame()
+      console.log(inputs)
+  }
 });
 
 const addNumInput = num => {
   for (let i = 0; i < num; i++) {
     let numbers = document.createElement("input");
+    numbers.value=0;
     numbers.setAttribute("type", "number");
     numbers.max=9;
     numbers.min=0;
@@ -26,6 +37,13 @@ const randomNumber= ()=>{
 const numToList = (num)=>{
     let list= num.toString().split('')
     
-    console.log(list)
+    return list
 }
-numToList(randomNumber())
+
+const playGame = ()=>{
+    if(inputs){
+       inputs.forEach(data=>console.log(data.value))
+ 
+    }
+
+}
