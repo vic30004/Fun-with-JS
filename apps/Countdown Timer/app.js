941b3a0c06a04d589266fc8931fff9f9f5d54c0c
vic30004 <victor_993@msn.com>
2020-01-07 17:45:50 +0200

    initial setup

---
+++ apps/Countdown Timer/app.js
@@ -0,0 +1,7 @@
+const endDate = document.querySelector("input[name='endDate']");
+
+const clock = document.querySelector(".clock");
+
+endDate.addEventListener("change", (e)=>{
+    e.preventDefault();
+})
