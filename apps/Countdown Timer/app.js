dc4e1edf4fa43fd2537b4bd3d27586881fb5bc6b
vic30004 <victor_993@msn.com>
2020-01-07 18:02:05 +0200

    added logic to calculate the time difference

--- apps/Countdown Timer/app.js
+++ apps/Countdown Timer/app.js
@@ -1,7 +1,35 @@
 const endDate = document.querySelector("input[name='endDate']");
 
-const clock = document.querySelector(".clock");
+const clock = document.querySelector('.clock');
 
-endDate.addEventListener("change", (e)=>{
-    e.preventDefault();
-})
+endDate.addEventListener('change', e => {
+  e.preventDefault();
+  console.log(endDate.value);
+  const temp = new Date();
+  console.log(temp);
+  startClock(temp);
+});
+
+const startClock = d => {
+  console.log(timeLeft(d));
+};
+
+const timeLeft = d => {
+  let currentDate = new Date();
+  // console.log(currentDate)
+  // console.log(Date.parse(currentDate))
+  let t = Date.parse(d) - Date.parse(currentDate);
+  console.log(t);
+  let seconds = Math.floor((t / 1000) % 60);
+  let minutes = Math.floor((t / 1000 / 60) % 60);
+  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  let days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  };
+};
