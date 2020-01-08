714b2f54f97a9f54fb1274aca63bd22f80cfb068
vic30004 <victor_993@msn.com>
2020-01-08 15:51:32 +0200

    added logic to track clicks and boxes clicked

--- apps/pattern-matching/app.js
+++ apps/pattern-matching/app.js
@@ -2,42 +2,54 @@ const message = document.querySelector('.message');
 const gameArea = document.querySelector('.gamearea');
 const button = document.querySelector('button');
 
-const gameColor = ["red","blue","green","orange"];
+const gameColor = ['red', 'blue', 'green', 'orange'];
+let gameClicks = [];
+let userClicks = [];
+let inPlay = false;
+let playNum = 1;
 
 button.addEventListener('click', () => {
-  console.log('clicked');
+  if (!inPlay) {
+    player();
+  }
 });
 
-
-const checkAnswer=(e)=>{
-    let el= e.target;
-    console.log(el)
+function player() {
+  button.disabled = true;
+  gameClicks = [];
+  userClicks = [];
+  runSequence();
 }
 
-const setup= ()=>{
-    console.log("Loaded");
-    for (let i=0;i<4;i++){
-        let div=eleFactory("div");
-        div.style.backgroundColor=gameColor[i];
-        div.classList.add("box");
-        div.style.opacity="0.5";
-        div.myColor = gameColor[i];
-        div.addEventListener("click",checkAnswer);
-        gameArea.appendChild(div);
-        
-    }
+function runSequence() {
+    inPlay=true;
 }
 
+const checkAnswer = e => {
+  if (inPlay) {
+    let el = e.target;
+    console.log(el);
+    userClicks.push(el.myColor);
+  }
+  console.log(userClicks);
+};
 
+const setup = () => {
+  console.log('Loaded');
+  for (let i = 0; i < 4; i++) {
+    let div = eleFactory('div');
+    div.style.backgroundColor = gameColor[i];
+    div.classList.add('box');
+    div.style.opacity = '0.5';
+    div.myColor = gameColor[i];
+    div.addEventListener('click', checkAnswer);
+    gameArea.appendChild(div);
+  }
+};
 
-window.addEventListener("load",setup);
+window.addEventListener('load', setup);
 
-
-
-
-
-const eleFactory=(elType)=>{
-    let ele = document.createElement(elType);
-    return ele;
-}
-
+const eleFactory = elType => {
+  let ele = document.createElement(elType);
+  return ele;
+};
