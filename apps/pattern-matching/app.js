27dc57870bac183023ec33d566b9e703f0e509e7
vic30004 <victor_993@msn.com>
2020-01-08 15:29:27 +0200

    page dynamically loads boxes

--- apps/pattern-matching/app.js
+++ apps/pattern-matching/app.js
@@ -2,6 +2,31 @@ const message = document.querySelector('.message');
 const gameArea = document.querySelector('.gamearea');
 const button = document.querySelector('button');
 
+const gameColor = ["red","blue","green","orange"];
+
 button.addEventListener('click', () => {
   console.log('clicked');
 });
+
+const setup= ()=>{
+    console.log("Loaded");
+    for (let i=0;i<4;i++){
+        let div=eleFactory("div");
+        div.style.backgroundColor=gameColor[i];
+        div.classList.add("box");
+        gameArea.appendChild(div);
+        
+    }
+}
+
+window.addEventListener("load",setup);
+
+
+
+
+
+const eleFactory=(elType)=>{
+    let ele = document.createElement(elType);
+    return ele;
+}
+
