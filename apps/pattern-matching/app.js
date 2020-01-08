0d5c5513f3a361a942f7d20504fb2a102c1f5558
vic30004 <victor_993@msn.com>
2020-01-08 15:45:53 +0200

    added more event listeners

--- apps/pattern-matching/app.js
+++ apps/pattern-matching/app.js
@@ -8,16 +8,27 @@ button.addEventListener('click', () => {
   console.log('clicked');
 });
 
+
+const checkAnswer=(e)=>{
+    let el= e.target;
+    console.log(el)
+}
+
 const setup= ()=>{
     console.log("Loaded");
     for (let i=0;i<4;i++){
         let div=eleFactory("div");
         div.style.backgroundColor=gameColor[i];
         div.classList.add("box");
+        div.style.opacity="0.5";
+        div.myColor = gameColor[i];
+        div.addEventListener("click",checkAnswer);
         gameArea.appendChild(div);
         
     }
 }
+
+
 
 window.addEventListener("load",setup);
 
