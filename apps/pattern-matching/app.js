4ea2c571fd85055cb98115e9326de90d2f3d5013
vic30004 <victor_993@msn.com>
2020-01-08 15:20:23 +0200

    added basic css,html and js

---
+++ apps/pattern-matching/app.js
@@ -0,0 +1,7 @@
+const message = document.querySelector('.message');
+const gameArea = document.querySelector('.gamearea');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  console.log('clicked');
+});
